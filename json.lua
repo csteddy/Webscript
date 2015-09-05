0de454551ff66210b881900557f24419fb05a8be
Craig Steddy <csteddy@petaplex.com>
2015-09-06 07:08:50 +0800

    Create json.lua

---
+++ json.lua
@@ -0,0 +1,13 @@
+json = {}
+local lunajson = require 'csteddy/lunajson/src/lunajson.lua'
+function json.parse(str)
+log('new parse function')
+return lunajson.decode(str)
+
+end
+
+function json.stringify(table)
+log('new serialise function'
+return lunajson.encode(table)
+
+end
