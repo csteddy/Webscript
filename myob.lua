2629592dd19043a63ac748ec9778c90028eb3202
Craig Steddy <csteddy@petaplex.com>
2015-10-15 15:24:40 +0800

    Update myob.lua

--- myob.lua
+++ myob.lua
@@ -45,6 +45,7 @@ local r = lib.callout(ep,'GET',{},{})
 if r.statuscode == 304 then
 	r.statuscode = 200
 	r.content = cache[ep].data
+	log('myob.lua '..r.statuscode)
 else if r.statuscode == 200 then
 
   cache[ep] = {['etag'] = r.headers['etag'],
