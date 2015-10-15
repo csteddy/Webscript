b393745b7f3e867e3579b260828e9bb828f96816
Craig Steddy <csteddy@petaplex.com>
2015-10-15 15:03:36 +0800

    Update myob.lua

--- myob.lua
+++ myob.lua
@@ -27,9 +27,9 @@ req_headers["x-myobapi-version"] = "v2"
 queue = 'myob'
 lease.acquire(queue)
 
-myob={}
+cache={}
 if storage[queue] then
-  myob=json.parse(storage[queue])
+  cache=json.parse(storage[queue])
 end
 
 --q=q.."?$filter=PayPeriodStartDate eq datetime'2015-09-03T00:00:00'"
@@ -37,18 +37,18 @@ ep = API_endpoint..q
 --check if we've called this endpoint before
 --if so then pass the etag that we have in the headers so that
 --MYOB can tell us if we already have the most current data
-if myob[ep] then
-	req_headers["If-None-Match"] = myob[ep].etag
+if cache[ep] then
+	req_headers["If-None-Match"] = cache[ep].etag
 end
 local r = lib.callout(ep,'GET',{},{})
 if r.statuscode == 304 then
 	r.statuscode = 200
-	r.content = myob[ep].data
+	r.content = cache[ep].data
 else if r.statuscode == 200 then
 
-  myob[ep] = {['etag'] = r.headers['etag'],
-							['data'] = r.content}
-  storage[queue]=json.stringify(myob)
+  cache[ep] = {['etag'] = r.headers['etag'],
+		['data'] = r.content}
+  storage[queue]=json.stringify(cache)
 	
 	
 	end
