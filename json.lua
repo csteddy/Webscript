1c03d62b9285cff106485f008249db631e2a4b0e
Craig Steddy <csteddy@petaplex.com>
2016-09-11 17:00:30 +0800

    Update json.lua

--- json.lua
+++ json.lua
@@ -1,13 +1,13 @@
 json = {}
 local lunajson = require 'csteddy/lunajson/src/lunajson.lua'
 function json.parse(str)
-log('new parse function')
+--log('new parse function')
 return lunajson.decode(str)
 
 end
 
 function json.stringify(table)
-log('new serialise function')
+--log('new serialise function')
 return lunajson.encode(table)
 
 end
